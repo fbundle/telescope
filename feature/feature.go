1a084c1fa59521b0cdbea17ac0c7d8f83e62cb41
khanh <khanh.nguyen.contact@gmail.com>
2025-07-25 00:25:23 +0800

    auto_push

--- feature/feature.go
+++ feature/feature.go
@@ -10,6 +10,10 @@ func ParallelIndexing() bool {
 	return len(os.Getenv("PARALLEL_INDEXING")) > 0
 }
 
+func Journaling() bool {
+	return len(os.Getenv("JOURNALING")) > 0
+}
+
 const (
 	JOURNAL_INTERVAL_S           = 60
 	DEBUG_IO_INTERVAL_MS         = 100
