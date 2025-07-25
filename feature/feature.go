533ad5280661ac4ca5a78315d2197530b1ca70d5
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-07-25 17:30:51 +0800

    auto_push

--- feature/feature.go
+++ feature/feature.go
@@ -8,10 +8,6 @@ func Debug() bool {
 	return len(os.Getenv("DEBUG")) > 0
 }
 
-func ParallelIndexing() bool {
-	return len(os.Getenv("PARALLEL_INDEXING")) > 0
-}
-
 const (
 	LOG_FLUSH_INTERVAL_S         = 60
 	DEBUG_IO_INTERVAL_MS         = 100
