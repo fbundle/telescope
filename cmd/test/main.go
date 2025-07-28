c39d1e3dc93c5e2fc31e65021487d87f91a15d60
khanh <khanh.nguyen.contact@gmail.com>
2025-07-28 23:11:12 +0800

    auto_push

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "telescope/exit"
+import (
+	"fmt"
+	"telescope/persistent/vector"
+)
 
 func main() {
-	exit.Write("heheh")
+	v := vector.New[int]()
+	v = v.Ins(v.Len(), 0)
+	v = v.Ins(v.Len(), 1)
+	v = v.Ins(v.Len(), 2)
+	v = v.Ins(v.Len(), 3)
+
+	for i, val := range v.Iter {
+		fmt.Println(i, val)
+		if i >= 2 {
+			break
+		}
+	}
 }
