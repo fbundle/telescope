76d90a8fabd1c68c4a05f8a94d32a792482c8ee7
khanh <khanh.nguyen.contact@gmail.com>
2025-08-02 00:43:05 +0800

    auto_push

--- util/persistent/vector/vector.go
+++ util/persistent/vector/vector.go
@@ -13,31 +13,31 @@ type Vector[T any] interface {
 	Repr() []T
 }
 
-func New[T any]() Vector[T] {
-	return &vector[T]{node: nil}
+func New[T any]() *WeightBalancedTree[T] {
+	return &WeightBalancedTree[T]{node: nil}
 }
 
-type vector[T any] struct {
+type WeightBalancedTree[T any] struct {
 	node *node[T]
 }
 
-func (v *vector[T]) Get(i int) T {
+func (v *WeightBalancedTree[T]) Get(i int) T {
 	return get(v.node, uint(i))
 }
 
-func (v *vector[T]) Set(i int, val T) Vector[T] {
-	return &vector[T]{node: set(v.node, uint(i), val)}
+func (v *WeightBalancedTree[T]) Set(i int, val T) Vector[T] {
+	return &WeightBalancedTree[T]{node: set(v.node, uint(i), val)}
 }
 
-func (v *vector[T]) Ins(i int, val T) Vector[T] {
-	return &vector[T]{node: ins(v.node, uint(i), val)}
+func (v *WeightBalancedTree[T]) Ins(i int, val T) Vector[T] {
+	return &WeightBalancedTree[T]{node: ins(v.node, uint(i), val)}
 }
 
-func (v *vector[T]) Del(i int) Vector[T] {
-	return &vector[T]{node: del(v.node, uint(i))}
+func (v *WeightBalancedTree[T]) Del(i int) Vector[T] {
+	return &WeightBalancedTree[T]{node: del(v.node, uint(i))}
 }
 
-func (v *vector[T]) Iter(f func(i int, val T) bool) {
+func (v *WeightBalancedTree[T]) Iter(f func(i int, val T) bool) {
 	i := 0
 	iter(v.node, func(val T) bool {
 		ok := f(i, val)
@@ -46,24 +46,24 @@ func (v *vector[T]) Iter(f func(i int, val T) bool) {
 	})
 }
 
-func (v *vector[T]) Len() int {
+func (v *WeightBalancedTree[T]) Len() int {
 	return int(weight(v.node))
 }
-func (v *vector[T]) Height() int {
+func (v *WeightBalancedTree[T]) Height() int {
 	return int(height(v.node))
 }
-func (v *vector[T]) Split(i int) (Vector[T], Vector[T]) {
+func (v *WeightBalancedTree[T]) Split(i int) (Vector[T], Vector[T]) {
 	n1, n2 := split(v.node, uint(i))
-	return &vector[T]{node: n1}, &vector[T]{node: n2}
+	return &WeightBalancedTree[T]{node: n1}, &WeightBalancedTree[T]{node: n2}
 }
 
-func (v *vector[T]) Concat(other Vector[T]) Vector[T] {
-	n1, n2 := v.node, other.(*vector[T]).node
+func (v *WeightBalancedTree[T]) Concat(other Vector[T]) Vector[T] {
+	n1, n2 := v.node, other.(*WeightBalancedTree[T]).node
 	n3 := merge(n1, n2)
-	return &vector[T]{node: n3}
+	return &WeightBalancedTree[T]{node: n3}
 }
 
-func (v *vector[T]) Repr() []T {
+func (v *WeightBalancedTree[T]) Repr() []T {
 	buffer := make([]T, 0, v.Len())
 	v.Iter(func(i int, val T) bool {
 		buffer = append(buffer, val)
