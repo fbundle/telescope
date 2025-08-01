3f1b3424ee18c83c118c4b018780575efe516d72
khanh <khanh.nguyen.contact@gmail.com>
2025-08-02 01:47:10 +0800

    auto_push

--- core/text/util.go
+++ core/text/util.go
@@ -2,32 +2,9 @@ package text
 
 import (
 	"context"
-	"slices"
 	"telescope/util/buffer"
-	"telescope/util/side_channel"
 )
 
-func padNewLine(line []byte) []byte {
-	if line[len(line)-1] == '\n' {
-		return line
-	}
-	return append(slices.Clone(line), '\n')
-}
-
-func endOfLineSize(line []byte) int {
-	if len(line) == 0 {
-		side_channel.Panic("empty line")
-	}
-	if line[len(line)-1] != '\n' {
-		side_channel.Panic("not end of line")
-	}
-	if len(line) >= 2 && line[len(line)-2] == '\r' {
-		// windows line ends with \r\n
-		return 2
-	}
-	// linux/macos line ends with \n
-	return 1
-}
 func LoadFile(ctx context.Context, reader buffer.Buffer, update func(Line, int)) error {
 	return indexFile(ctx, reader, '\n', func(offset int, line []byte) {
 		l := makeLineFromFile(offset)
