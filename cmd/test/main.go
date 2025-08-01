fadab3af924be440fcd721c0e8dd37900bc20745
khanh <khanh.nguyen.contact@gmail.com>
2025-08-01 23:22:19 +0800

    auto_push

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -4,12 +4,34 @@ import (
 	"context"
 	"fmt"
 	"runtime"
-	"telescope/util/buffer"
+	"sync"
+	"sync/atomic"
 )
+
+type Chunk[T any] struct {
+	raw int64 // 8 bytes
+}
+
+var pool = &sync.Map{} // map[uint64]any
+var lastKey = int64(0)
+
+func NewChunkFromData[T any](data T, cancel func()) *Chunk[T] {
+	key := atomic.AddInt64(&lastKey, -1)
+	pool.Store(key, data)
+	line := &Chunk[T]{
+		raw: key,
+	}
+	runtime.AddCleanup(line, func(key int64) {
+		defer cancel()
+		pool.Delete(key)
+		fmt.Printf("key %d was cleaned\n", key)
+	}, key)
+	return line
+}
 
 func test() context.Context {
 	ctx, cancel := context.WithCancel(context.Background())
-	x := buffer.NewChunkFromData[[]byte]([]byte{1, 2, 3}, cancel)
+	x := NewChunkFromData[[]byte]([]byte{1, 2, 3}, cancel)
 	_ = x
 	return ctx
 }
