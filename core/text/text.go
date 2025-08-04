b9a54be4dbe199fd3da27c713674ca6041e3708b
khanh <khanh.nguyen.contact@gmail.com>
2025-08-04 23:38:01 +0800

    auto_push

--- core/text/text.go
+++ core/text/text.go
@@ -6,89 +6,77 @@ import (
 	"telescope/util/side_channel"
 )
 
-type Text interface {
-	Get(i int) []rune
-	Set(i int, val []rune) Text
-	Ins(i int, val []rune) Text
-	Del(i int) Text
-	Iter(f func(i int, val []rune) bool)
-	Len() int
-	Split(i int) (Text, Text)
-	Concat(t2 Text) Text
-	Append(line Line) Text
-}
-
 func New(reader buffer.Buffer) Text {
-	return text{
+	return Text{
 		Reader: reader,
 		Lines:  sequence.New[Line](),
 	}
 }
 
-type text struct {
+type Text struct {
 	Reader buffer.Buffer
 	Lines  sequence.Seq[Line]
 }
 
-func (t text) Append(line Line) Text {
-	return text{
+func (t Text) Append(line Line) Text {
+	return Text{
 		Reader: t.Reader,
 		Lines:  t.Lines.Ins(t.Lines.Len(), line),
 	}
 }
 
-func (t text) Get(i int) []rune {
+func (t Text) Get(i int) []rune {
 	return t.Lines.Get(i).Repr(t.Reader)
 }
 
-func (t text) Set(i int, val []rune) Text {
-	return text{
+func (t Text) Set(i int, val []rune) Text {
+	return Text{
 		Reader: t.Reader,
 		Lines:  t.Lines.Set(i, makeLineFromData(val)),
 	}
 }
 
-func (t text) Ins(i int, val []rune) Text {
-	return text{
+func (t Text) Ins(i int, val []rune) Text {
+	return Text{
 		Reader: t.Reader,
 		Lines:  t.Lines.Ins(i, makeLineFromData(val)),
 	}
 }
 
-func (t text) Del(i int) Text {
-	return text{
+func (t Text) Del(i int) Text {
+	return Text{
 		Reader: t.Reader,
 		Lines:  t.Lines.Del(i),
 	}
 }
 
-func (t text) Iter(f func(i int, val []rune) bool) {
+func (t Text) Iter(f func(i int, val []rune) bool) {
 	t.Lines.Iter(func(i int, l Line) bool {
 		return f(i, l.Repr(t.Reader))
 	})
 }
 
-func (t text) Len() int {
+func (t Text) Len() int {
 	return t.Lines.Len()
 }
 
-func (t text) Split(i int) (Text, Text) {
+func (t Text) Split(i int) (Text, Text) {
 	v1, v2 := t.Lines.Split(i)
-	return text{
+	return Text{
 			Reader: t.Reader,
 			Lines:  v1,
-		}, text{
+		}, Text{
 			Reader: t.Reader,
 			Lines:  v2,
 		}
 }
 
-func (t text) Concat(t2 Text) Text {
-	if t.Reader != t2.(text).Reader {
+func (t Text) Concat(t2 Text) Text {
+	if t.Reader != t2.Reader {
 		side_channel.Panic("different readers")
 	}
-	return text{
+	return Text{
 		Reader: t.Reader,
-		Lines:  t.Lines.Concat(t2.(text).Lines),
+		Lines:  t.Lines.Concat(t2.Lines),
 	}
 }
