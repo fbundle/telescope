eb890e079d82a637d02e601a94ba3b2d3ff560da
khanh <khanh.nguyen.contact@gmail.com>
2025-08-06 21:30:00 +0800

    auto_push

--- util/persistent/ordered_map/ordered_map.go
+++ util/persistent/ordered_map/ordered_map.go
@@ -4,7 +4,7 @@ import "golang.org/x/exp/constraints"
 
 func NewOrderedMap[K constraints.Ordered, V any]() OrderedMap[K, V] {
 	return OrderedMap[K, V]{
-		Map: Empty[Entry[K, V]](),
+		comparableMap: Empty[Entry[K, V]](),
 	}
 }
 
@@ -25,48 +25,48 @@ func (e Entry[K, V]) Cmp(o Entry[K, V]) int {
 }
 
 type OrderedMap[K constraints.Ordered, V any] struct {
-	Map Map[Entry[K, V]]
+	comparableMap Map[Entry[K, V]]
 }
 
 func (m OrderedMap[K, V]) Get(key K) (V, bool) {
-	entryOut, ok := m.Map.Get(Entry[K, V]{Key: key})
+	entryOut, ok := m.comparableMap.Get(Entry[K, V]{Key: key})
 	return entryOut.(Entry[K, V]).Val, ok
 }
 
 func (m OrderedMap[K, V]) Set(key K, val V) OrderedMap[K, V] {
 	return OrderedMap[K, V]{
-		Map: m.Map.Set(Entry[K, V]{Key: key, Val: val}),
+		comparableMap: m.comparableMap.Set(Entry[K, V]{Key: key, Val: val}),
 	}
 }
 
 func (m OrderedMap[K, V]) Del(key K) OrderedMap[K, V] {
 	return OrderedMap[K, V]{
-		Map: m.Map.Del(Entry[K, V]{Key: key}),
+		comparableMap: m.comparableMap.Del(Entry[K, V]{Key: key}),
 	}
 }
 
 func (m OrderedMap[K, V]) Iter(f func(K, V) bool) {
-	m.Map.Iter(func(entryOut Entry[K, V]) bool {
+	m.comparableMap.Iter(func(entryOut Entry[K, V]) bool {
 		return f(entryOut.Key, entryOut.Val)
 	})
 }
 
 func (m OrderedMap[K, V]) Len() int {
-	return int(m.Map.Weight())
+	return int(m.comparableMap.Weight())
 }
 
 func (m OrderedMap[K, V]) Split(key K) (OrderedMap[K, V], OrderedMap[K, V]) {
-	m1, m2 := m.Map.Split(Entry[K, V]{Key: key})
-	return OrderedMap[K, V]{Map: m1}, OrderedMap[K, V]{Map: m2}
+	m1, m2 := m.comparableMap.Split(Entry[K, V]{Key: key})
+	return OrderedMap[K, V]{comparableMap: m1}, OrderedMap[K, V]{comparableMap: m2}
 }
 
 func (m OrderedMap[K, V]) Max() (K, V) {
-	entryOut := m.Map.Max().(Entry[K, V])
+	entryOut := m.comparableMap.Max().(Entry[K, V])
 	return entryOut.Key, entryOut.Val
 }
 
 func (m OrderedMap[K, V]) Min() (K, V) {
-	entryOut := m.Map.Min().(Entry[K, V])
+	entryOut := m.comparableMap.Min().(Entry[K, V])
 	return entryOut.Key, entryOut.Val
 }
 
