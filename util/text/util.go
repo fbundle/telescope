3a16b1658176c7ff72fbe0202efa8eead0ce2d06
khanh <khanh.nguyen.contact@gmail.com>
2025-08-06 21:05:13 +0800

    auto_push

--- util/text/util.go
+++ util/text/util.go
@@ -1,44 +1,11 @@
 package text
 
 import (
-	"context"
 	"telescope/util/buffer"
 	"telescope/util/persistent/seq"
 )
 
 const delim byte = '\n'
-
-func LoadFile(ctx context.Context, reader buffer.Reader, update func(Line, int)) error {
-	return indexFile(ctx, reader, func(offset int, line []byte) {
-		l := makeLineFromOffset(offset)
-		update(l, len(line))
-	})
-}
-
-func indexFile(ctx context.Context, reader buffer.Reader, update func(offset int, line []byte)) error {
-	var offset int = 0
-
-	var line []byte
-
-	for i := 0; i < reader.Len(); i++ {
-		select {
-		case <-ctx.Done():
-			return nil
-		default:
-		}
-
-		b := reader.At(i)
-		line = append(line, b)
-		if b == delim {
-			update(offset, line) // line with delim
-			offset, line = i+1, nil
-		}
-	}
-	if len(line) > 0 {
-		update(offset, line)
-	}
-	return nil
-}
 
 func GetLinesFromSeq(reader buffer.Reader, lines seq.Seq[Line]) [][]rune {
 	out := make([][]rune, 0, lines.Len())
