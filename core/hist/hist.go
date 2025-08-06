89e9daa814707d045267507a42b84d122a82640c
khanh <khanh.nguyen.contact@gmail.com>
2025-08-06 20:36:59 +0800

    auto_push

--- core/hist/hist.go
+++ core/hist/hist.go
@@ -6,37 +6,37 @@ import (
 
 func New[T any](t T) *Hist[T] {
 	return &Hist[T]{
-		i:  0,
-		ts: []T{t},
+		LatestVersion: 0,
+		Stack:         []T{t},
 	}
 }
 
 type Hist[T any] struct {
-	i  int // current version
-	ts []T // all versions
+	LatestVersion uint64 // current version
+	Stack         []T    // all versions
 }
 
 func (h *Hist[T]) Update(modifier func(T) T) {
-	next := modifier(h.ts[h.i])
-	h.ts = append(h.ts[:h.i+1], next)
-	h.i++
-	if len(h.ts) > config.Load().MAXSIZE_HISTORY {
-		h.ts = h.ts[1:]
-		h.i--
+	next := modifier(h.Stack[h.LatestVersion])
+	h.Stack = append(h.Stack[:h.LatestVersion+1], next)
+	h.LatestVersion++
+	if len(h.Stack) > config.Load().MAXSIZE_HISTORY {
+		h.Stack = h.Stack[1:]
+		h.LatestVersion--
 	}
 }
 
 func (h *Hist[T]) Get() T {
-	return h.ts[h.i]
+	return h.Stack[h.LatestVersion]
 }
 func (h *Hist[T]) Undo() {
-	if h.i > 0 {
-		h.i--
+	if h.LatestVersion > 0 {
+		h.LatestVersion--
 	}
 }
 
 func (h *Hist[T]) Redo() {
-	if h.i < len(h.ts)-1 {
-		h.i++
+	if int(h.LatestVersion) < len(h.Stack)-1 {
+		h.LatestVersion++
 	}
 }
