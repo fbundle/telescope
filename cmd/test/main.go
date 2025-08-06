6d4cf08910ff9ab59884bbf3f5d6864ca14b7ba5
khanh <khanh.nguyen.contact@gmail.com>
2025-08-07 00:11:40 +0800

    auto_push

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"telescope/util/monad"
 )
 
 func main() {
 	m := monad.Unit[int](2, 3, 4)
-	for v := range m.Chan {
-		fmt.Println(v)
-	}
-	if m.Error != nil {
-		fmt.Println(m.Error)
-	}
+	m = monad.Bind(m, func(t1 int) (*monad.Monad[int], error) {
+		if t1 == 4 {
+			return nil, errors.New("my_error")
+		}
+		ts := make([]int, 0)
+		for i := 0; i < t1; i++ {
+			ts = append(ts, t1)
+		}
+		return monad.Unit[int](ts...), nil
+	})
+	fmt.Println(m.Unwrap())
 }
--- util/monad/monad.go
+++ util/monad/monad.go
@@ -5,33 +5,51 @@ type Monad[T any] struct {
 	Error error
 }
 
-func Unit[T any](vs ...T) Monad[T] {
+func (m *Monad[T]) Unwrap() ([]T, error) {
+	var vs []T
+	for v := range m.Chan {
+		vs = append(vs, v)
+	}
+	return vs, m.Error
+}
+
+func None[T any]() *Monad[T] {
+	return Unit[T]()
+}
+
+func Unit[T any](vs ...T) *Monad[T] {
 	ch := make(chan T, 1)
+	m := &Monad[T]{
+		Chan:  ch,
+		Error: nil,
+	}
 	go func() {
 		for _, v := range vs {
 			ch <- v
 		}
 		close(ch)
 	}()
-	return Monad[T]{
+	return m
+}
+
+func Bind[T1 any, T2 any](m1 *Monad[T1], f func(T1) (*Monad[T2], error)) *Monad[T2] {
+	ch := make(chan T2, 1)
+	m := &Monad[T2]{
 		Chan:  ch,
 		Error: nil,
 	}
-}
-
-func Bind[T1 any, T2 any](m1 Monad[T1], f func(T1) Monad[T2]) Monad[T2] {
-	ch := make(chan T2, 1)
 	go func() {
 		for v := range m1.Chan {
-			m2 := f(v)
+			m2, err := f(v)
+			if err != nil {
+				m.Error = err
+				break
+			}
 			for v2 := range m2.Chan {
 				ch <- v2
 			}
 		}
 		close(ch)
 	}()
-	return Monad[T2]{
-		Chan:  ch,
-		Error: nil,
-	}
+	return m
 }
