35983f24b4d690bb9fdb934499ae307d72813019
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-08-12 15:21:47 +0800

    auto_push

---
+++ ui/draw.go
@@ -0,0 +1,17 @@
+package ui
+
+import "github.com/gdamore/tcell/v2"
+
+type drawFunc = func(x int, y int, primary rune, combining []rune, style tcell.Style)
+type drawCtx = func(width int, height int, draw drawFunc)
+
+// makeDrawContext - draw into a rectangle (offsetX int, offsetY int, width int, height int)
+func makeDrawContext(s tcell.Screen, offsetX int, offsetY int, width int, height int) func(drawCtx) {
+	return func(f func(width int, height int, draw drawFunc)) {
+		f(width, height, func(x int, y int, primary rune, combining []rune, style tcell.Style) {
+			if 0 <= x && x < width && 0 <= y && y < height {
+				s.SetContent(offsetX+x, offsetY+y, primary, combining, style)
+			}
+		})
+	}
+}
--- ui/editor.go
+++ ui/editor.go
@@ -49,20 +49,6 @@ func getSelector(m map[string]any) *multimode_editor.Selector {
 	return selector
 }
 
-type drawFunc = func(x int, y int, primary rune, combining []rune, style tcell.Style)
-type drawCtx = func(width int, height int, draw drawFunc)
-
-// makeDrawContext - draw into a rectangle (offsetX int, offsetY int, width int, height int)
-func makeDrawContext(s tcell.Screen, offsetX int, offsetY int, width int, height int) func(drawCtx) {
-	return func(f func(width int, height int, draw drawFunc)) {
-		f(width, height, func(x int, y int, primary rune, combining []rune, style tcell.Style) {
-			if 0 <= x && x < width && 0 <= y && y < height {
-				s.SetContent(offsetX+x, offsetY+y, primary, combining, style)
-			}
-		})
-	}
-}
-
 func getStatusStyle(mode string) tcell.Style {
 	switch mode {
 	case multimode_editor.ModeNormal:
