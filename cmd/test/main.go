2e59180a55b890bd86815423b611cb5297eefe19
khanh <khanh.nguyen.contact@gmail.com>
2025-08-15 00:46:48 +0800

    auto_push

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"os/signal"
+	"syscall"
+	"telescope/util/unbounded"
+)
+
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	ch := unbounded.New[int](ctx)
+
+	go func() {
+		for i := 0; i < 1_000_000_000; i++ {
+			ch.InChan() <- i
+		}
+		close(ch.InChan())
+	}()
+
+	for i := range ch.OutChan() {
+		_ = i
+	}
 
 }
