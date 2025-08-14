cf73f7aebf07b42146e23499dc087f55057782fd
khanh <khanh.nguyen.contact@gmail.com>
2025-08-14 21:28:27 +0800

    auto_push

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"telescope/util/iterator"
-)
-
-func echo(ch iterator.Iterator[int]) {
-	for i := range ch {
-		fmt.Println(i)
-	}
-}
-
 func main() {
-	echo(func(f func(int) bool) {
-		for i := 0; i < 10; i++ {
-			f(i)
-		}
-	})
+
 }
