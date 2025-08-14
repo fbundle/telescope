54096e552a1d3fd56949ccf2f936541878fa9e7e
khanh <khanh.nguyen.contact@gmail.com>
2025-08-14 21:25:32 +0800

    auto_push

--- util/text/load.go
+++ util/text/load.go
@@ -1,13 +1,11 @@
 package text
 
 import (
-	"context"
 	"iter"
-	"telescope/config"
 	"telescope/util/buffer"
 )
 
-func IndexFile2(reader buffer.Reader) iter.Seq2[int, int] {
+func IndexFile(reader buffer.Reader) iter.Seq2[int, int] {
 	return func(yield func(index int, offset int) bool) {
 		index, offset := 0, 0
 		for i := 0; i < reader.Len(); i++ {
@@ -23,31 +21,4 @@ func IndexFile2(reader buffer.Reader) iter.Seq2[int, int] {
 			yield(index, offset)
 		}
 	}
-}
-
-func IndexFile(ctx context.Context, reader buffer.Reader, update func(offset int, line []byte)) error {
-	var offset int = 0
-	var line []byte = nil
-
-	for i := 0; i < reader.Len(); i++ {
-		if i%config.Load().LOAD_ESCAPE_INTERVAL == 0 {
-			// check every 10MB
-			select {
-			case <-ctx.Done():
-				return nil
-			default:
-			}
-		}
-
-		b := reader.At(i)
-		line = append(line, b)
-		if line[len(line)-1] == delim {
-			update(offset, line) // line with delim
-			offset, line = i+1, nil
-		}
-	}
-	if len(line) > 0 {
-		update(offset, line)
-	}
-	return nil
 }
