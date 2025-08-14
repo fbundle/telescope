aab5018a6fdfd9dff29fb4701e2e5218d36bdee3
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-08-14 12:43:10 +0800

    auto_push

--- ui/draw.go
+++ ui/draw.go
@@ -3,10 +3,10 @@ package ui
 import "github.com/gdamore/tcell/v2"
 
 type drawFunc = func(x int, y int, primary rune, combining []rune, style tcell.Style)
-type drawCtx = func(width int, height int, draw drawFunc)
+type drawCtx = func(func(width int, height int, draw drawFunc))
 
 // makeDrawContext - draw into a rectangle (offsetX int, offsetY int, width int, height int)
-func makeDrawContext(s tcell.Screen, offsetX int, offsetY int, width int, height int) func(drawCtx) {
+func makeDrawContext(s tcell.Screen, offsetX int, offsetY int, width int, height int) drawCtx {
 	return func(f func(width int, height int, draw drawFunc)) {
 		f(width, height, func(x int, y int, primary rune, combining []rune, style tcell.Style) {
 			if 0 <= x && x < width && 0 <= y && y < height {
