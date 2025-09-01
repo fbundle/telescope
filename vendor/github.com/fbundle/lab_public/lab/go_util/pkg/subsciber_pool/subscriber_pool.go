7ac2db2426da1638ba3a89ae2c220765fbd0f2a5
khanh <khanh.nguyen.contact@gmail.com>
2025-09-02 00:15:24 +0800

    auto_push

---
+++ vendor/github.com/fbundle/lab_public/lab/go_util/pkg/subsciber_pool/subscriber_pool.go
@@ -0,0 +1,33 @@
+package subsciber_pool
+
+import (
+	"sync/atomic"
+
+	"github.com/fbundle/lab_public/lab/go_util/pkg/sync_util"
+)
+
+type Pool[T any] struct {
+	handlerMap *sync_util.Map[uint64, T]
+	lastKey    uint64
+}
+
+func New[T any]() *Pool[T] {
+	return &Pool[T]{
+		handlerMap: &sync_util.Map[uint64, T]{},
+		lastKey:    0,
+	}
+}
+
+func (p *Pool[T]) Subscribe(a T) uint64 {
+	key := atomic.AddUint64(&p.lastKey, 1)
+	p.handlerMap.Store(key, a)
+	return key
+}
+
+func (p *Pool[T]) Unsubscribe(key uint64) {
+	p.handlerMap.Delete(key)
+}
+
+func (p *Pool[T]) Iter(f func(k uint64, v T) bool) {
+	p.handlerMap.Range(f)
+}
